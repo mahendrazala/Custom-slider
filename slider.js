b385b57d0289dee9e38b56b2a0ad9f616ef82bec
rinku zala <mahendrazala945@gmail.com>
2023-01-28 23:56:46 +0530

    ..

--- slider.js
+++ slider.js
@@ -1,64 +1,86 @@
-console.log('working');
-const left = document.querySelector('.left');
-const right = document.querySelector('.right');
-const slider = document.querySelector('.slider');
-const images = document.querySelectorAll('.image');
-const btn1 = document.querySelector('#btn1');
-const btn2 = document.querySelector('#btn2');
-const btn3 = document.querySelector('#btn3');
-const btn4 = document.querySelector('#btn4');
+console.log("working");
+const left = document.querySelector(".left");
+const right = document.querySelector(".right");
+const slider = document.querySelector(".slider");
+const images = document.querySelectorAll(".image");
+const button = document.querySelectorAll(".button");
 
 let sliderNumber = 1;
 length = images.length;
 
+
+button[0].style.backgroundColor = 'white';
+const resetBg = () => {
+    button.forEach((button) => {
+      button.style.backgroundColor = "transparent";
+    });
+  };
+  
+  button.forEach((button, i) => {
+    button.addEventListener("click", () => {
+  
+      resetBg();
+      slider.style.transform = `translateX(-${i * 800}px)`;
+      sliderNumber = i ?  button.style.backgroundColor = "white" : resetBg();
+     
+    });
+  });
+  
+  left.addEventListener("click", () => {
+    sliderNumber > 1 ? prevSlide() : lastSlide();
+    resetBg;
+    button[sliderNumber-1].style.backgroundColor = "white";
+
+
+  });
+
 const nextSlide = () => {
-    slider.style.transform = `translateX(-${sliderNumber * 800}px)`;
-    sliderNumber++;
-}
+  slider.style.transform = `translateX(-${sliderNumber * 800}px)`;
+  sliderNumber++;
+};
 
-const prevSlide  = () => {
-    slider.style.transform = `translateX(-${(sliderNumber - 2) * 800}px)`;
-    sliderNumber--;
-}
+const prevSlide = () => {
+  slider.style.transform = `translateX(-${(sliderNumber - 2) * 800}px)`;
+  sliderNumber--;
+};
 
-const firstSlide  = () => {
-    slider.style.transform = `translateX(0px)`;
-    sliderNumber = 1;
-}
-
+const firstSlide = () => {
+  slider.style.transform = `translateX(0px)`;
+  sliderNumber = 1;
+};
 
 const lastSlide = () => {
-    slider.style.transform = `translateX(-${(length - 1) * 800}px)`;
-    sliderNumber = length;
-}
+  slider.style.transform = `translateX(-${(length - 1) * 800}px)`;
+  sliderNumber = length;
+};
+
+right.addEventListener("click", () => {
+  sliderNumber < length ? nextSlide() : firstSlide();
+  resetBg();
+  button[sliderNumber-1].style.backgroundColor = "white";
+
+});
 
 
-right.addEventListener('click', () => {
-    sliderNumber < length ? nextSlide() : firstSlide();
-})
+// btn1.addEventListener('click',() => {
+//     slider.style.transform = `translateX(0px)`;
+//     btn1.classList.add('active')
+// })
 
+// btn2.addEventListener('click',() => {
+//     slider.style.transform = `translateX(-${1 * 800}px)`;
+//     btn2.classList.add('active');
 
+// })
 
-left.addEventListener('click', () => {
-   sliderNumber > 1 ? prevSlide() :lastSlide();
-})
+// btn3.addEventListener('click',() => {
+//        slider.style.transform = `translateX(-${2 * 800}px)`;
+//        btn3.classList.add('active')
 
-btn1.addEventListener('click',() => {
-    slider.style.transform = `translateX(0px)`;
-    console.log('sjfdb')
-})
+// })
 
-btn2.addEventListener('click',() => {
-    slider.style.transform = `translateX(-${1 * 800}px)`;
+// btn4.addEventListener('click',() => {
+//        slider.style.transform = `translateX(-${3 * 800}px)`;
+//        btn4.classList.add('active');
 
-})
-
-btn3.addEventListener('click',() => {
-       slider.style.transform = `translateX(-${2 * 800}px)`;
-
-})
-
-btn4.addEventListener('click',() => {
-       slider.style.transform = `translateX(-${3 * 800}px)`;
-
-})
+// })
